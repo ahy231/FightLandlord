5555062d1e9a884c3d60f3a7262228587af5cb42
unknown <zhongnanhai231@126.com>
2020-10-28 15:11:48 +0800

    update

--- src/com/dorm604/Dao/Dao.java
+++ src/com/dorm604/Dao/Dao.java
@@ -9,5 +9,21 @@ public interface Dao {
 	 * @param name 用户名
 	 * @return 用户信息
 	 */
-	UserBean getUserBean(String name);
+	public UserBean getUserBean(String name);
+	
+	/**
+	 * 	为用户交换欢乐豆
+	 * @param user 用户信息
+	 * @param amount 豆子数量
+	 * @return 函数执行状态，true：正常，false：异常
+	 */
+	public Boolean exchangeHappybeans(UserBean user, int amount);
+	
+	/**
+	 * 	为用户交换账户余额
+	 * @param user 用户信息
+	 * @param amount 豆子数量
+	 * @return 函数执行状态，true：正常，false：异常
+	 */
+	public Boolean exchangeMoney(UserBean user, int amount);
 }
